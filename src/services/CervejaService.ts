e16523f6e68f06d47d41fa8c3fe7d842e19f044b
Roger Zanelato <roger.zanelato@ambevtech.com.br>
2021-09-21 00:04:58 -0300

    configurado build

--- src/services/CervejaService.ts
+++ src/services/CervejaService.ts
@@ -1,20 +1,15 @@
-import { promises as fs } from 'fs';
-import path from 'path';
 import { CervejaResponse } from "../models/CervejaResponse";
 import { MarcaResponse } from "../models/MarcaResponse";
-
-const cervejasPath = path.join(__dirname, "..", "database", "cervejas.json");
-const marcasPath = path.join(__dirname, "..", "database", "marcas.json");
+import cervejas from '../database/cervejas.json';
+import marcas from '../database/marcas.json';
 
 class CervejaService {
     async obterCervejas(): Promise<CervejaResponse[]> {
-        const data = await fs.readFile(cervejasPath, 'utf-8');
-        return JSON.parse(data);
+        return cervejas;
     }
 
     async obterMarcas(): Promise<MarcaResponse[]> {
-        const data = await fs.readFile(marcasPath, 'utf-8');
-        return JSON.parse(data);
+        return marcas;
     }
 
     async obterCervejaPorMarca(marcaId: string): Promise<CervejaResponse[]> {
